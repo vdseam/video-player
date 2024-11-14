cdbdbd8c8dcede4d71bf5b9e7e51070a2f43420b
Vladyslav Deba <vdseam@gmail.com>
2024-11-14 14:43:48 +0100

    Initial Commit

---
+++ video-player/video_playerApp.swift
@@ -0,0 +1,17 @@
+//
+//  video_playerApp.swift
+//  video-player
+//
+//  Created by Vladyslav Deba on 14/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct video_playerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
